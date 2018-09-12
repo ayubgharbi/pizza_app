df9e16708bbbecc651856c705e51fcc5a14c9d52
Gharbi Ayoub <ayoub97ma@gmail.com>
2018-09-12 18:56:17 +0100

    add validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
+	validates :title, :description, :image, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
 	has_attached_file :image, styles: { large: "600x600>" }, 
 		:path => ":rails_root/public/system/:image/:id/:style/:filename",
     	:url => "/system/:image/:id/:style/:filename"
